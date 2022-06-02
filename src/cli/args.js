51012d74440a26c2924c59d433a5f7290fc54472
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-02 20:22:16 +0300

    feat: complete cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const argsWithoutPath = process.argv.slice(2);
+    let res = []
+
+    for (let i = 0; i < argsWithoutPath.length; i++) {
+        if (i % 2 === 0) {
+            res.push(`${argsWithoutPath[i].slice(2)} is ${argsWithoutPath[i + 1]}`);
+        }
+    }
+
+    console.log(res.join(', '))
 };
+
+parseArgs()
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,15 @@
 export const parseEnv = () => {
-    // Write your code here 
-};
+    const resRSSEnvs = [];
+
+    const arrayOfEnvs = Object.entries(process.env)
+
+    for (let i = 0; i < arrayOfEnvs.length; i++) {
+        if (arrayOfEnvs[i][0].startsWith('RSS_')) {
+            resRSSEnvs.push(`${arrayOfEnvs[i][0]}=${arrayOfEnvs[i][1]}`)
+        }
+    }
+
+    console.log(resRSSEnvs.join('; '))
+}
+
+parseEnv()
