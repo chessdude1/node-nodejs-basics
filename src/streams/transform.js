b6381d01964c9dca755a6e6023314de1ee81ac6a
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-03 14:09:45 +0300

    feat: streams transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,10 +1,6 @@
 import { Transform } from 'node:stream';
-import { createWriteStream, createReadStream } from 'fs'
 
 export const transform = async () => {
-    const readStream = createReadStream('./files/fileToRead.txt')
-    const writeStream = createWriteStream('./files/fileToWrite.txt')
-
     const reverseStream = new Transform({
         transform(data, encoding, callback) {
             const reversedData = data.toString().split("").reverse().join("");
@@ -13,10 +9,10 @@ export const transform = async () => {
         }
     });
 
-
-    readStream.pipe(reverseStream).pipe(writeStream).on('finish', (data, err) => {
-        console.log(data);
+    process.stdin.pipe(reverseStream).on('data', data => {
+        process.stdout.write(data.toString())
     });
+
 };
 
 transform()
