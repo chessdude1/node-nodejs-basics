6fb506f9a4fdc18848a9b8425ca57b645a0022fa
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-03 17:23:29 +0300

    feat: workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,39 @@
+import { cpus } from 'os'
+import { Worker } from 'worker_threads'
+
 export const performCalculations = async () => {
-    // Write your code here
+    const numberOfCores = cpus().length;
+
+    const workerResults = []
+
+    for (let i = 10; i < 10 + numberOfCores; i++) {
+        (workerResults.push(
+            new Promise((resolve, reject) => {
+                const worker = new Worker('./worker.js', {
+                    workerData: i
+                });
+                worker.on('message', data =>
+                    resolve(
+                        {
+                            status: 'resolved',
+                            data
+                        }
+                    ));
+                worker.on('error', () => reject(
+                    {
+                        status: 'error',
+                        data: null
+                    }
+                ));
+            }
+            )))
+    }
+    try {
+        console.log(await Promise.all(workerResults))
+    } catch (e) {
+        console.log(e)
+    }
+
 };
+
+performCalculations()
