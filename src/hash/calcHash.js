3226fb5ebbe95714b2db5805f40594be08210cda
chessdude1 <mitia.platonow2011@yandex.ru>
2022-06-03 13:58:34 +0300

    feat: streams

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const promise = await readFile('./files/fileToCalculateHashFor.txt', err => {
+        if (err) throw 'FS operation failed'
+    })
+
+    let hash = createHash('sha256').update(promise).digest('hex');
+    console.log(hash)
 };
+
+calculateHash()
